7e875412ec82d0040dfac985a1f89bf3595862e5
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-11-22 00:49:30 -0300

    Quedan funcionales los endpoints

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.json())
 
 app.get('/sell/:nombre', (req, res)=>{
     const nombreArchivo = req.params.nombre;
-    const archivoPath = path.join(__dirname, 'data', `${nombreArchivo}.json`);
+    const archivoPath = path.join(__dirname, 'sell', `${nombreArchivo}.json`);
 
 
-
+    
 
     fs.readFile(archivoPath, 'utf-8', (err, data)=>{
         if (err){
@@ -23,7 +23,96 @@ app.get('/sell/:nombre', (req, res)=>{
     })
 })
 
+app.get('/products/:nombre', (req, res)=>{
+    const nombreArchivo = req.params.nombre;
+    const archivoPath = path.join(__dirname, 'products', `${nombreArchivo}.json`);
+
+
+    
+
+    fs.readFile(archivoPath, 'utf-8', (err, data)=>{
+        if (err){
+            return res.status(404).send('Error al recuperar archivo')
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/products_comments/:nombre', (req, res)=>{
+    const nombreArchivo = req.params.nombre;
+    const archivoPath = path.join(__dirname, 'products_comments', `${nombreArchivo}.json`);
+
+
+    
+
+    fs.readFile(archivoPath, 'utf-8', (err, data)=>{
+        if (err){
+            return res.status(404).send('Error al recuperar archivo')
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/user_cart/:nombre', (req, res)=>{
+    const nombreArchivo = req.params.nombre;
+    const archivoPath = path.join(__dirname, 'user_cart', `${nombreArchivo}.json`);
+
+
+    
+
+    fs.readFile(archivoPath, 'utf-8', (err, data)=>{
+        if (err){
+            return res.status(404).send('Error al recuperar archivo')
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/cats_products/:nombre', (req, res)=>{
+    const nombreArchivo = req.params.nombre;
+    const archivoPath = path.join(__dirname, 'cats_products', `${nombreArchivo}.json`);
+
+
+    
+
+    fs.readFile(archivoPath, 'utf-8', (err, data)=>{
+        if (err){
+            return res.status(404).send('Error al recuperar archivo')
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/cats/:nombre', (req, res)=>{
+    const nombreArchivo = req.params.nombre;
+    const archivoPath = path.join(__dirname, 'cats', `${nombreArchivo}.json`);
+
+
+    
+
+    fs.readFile(archivoPath, 'utf-8', (err, data)=>{
+        if (err){
+            return res.status(404).send('Error al recuperar archivo')
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/cart/:nombre', (req, res)=>{
+    const nombreArchivo = req.params.nombre;
+    const archivoPath = path.join(__dirname, 'cart', `${nombreArchivo}.json`);
+
+
+    
+
+    fs.readFile(archivoPath, 'utf-8', (err, data)=>{
+        if (err){
+            return res.status(404).send('Error al recuperar archivo')
+        }
+        res.json(JSON.parse(data))
+    })
+})
 
 app.listen(port, () =>{
-    console.log(`Servidor corriendo en http://localhost:{$port}`)
+    console.log(`Servidor corriendo en http://localhost:${port}`)
 })
