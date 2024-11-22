023f0c6e95a54d497048cdd2438d710f13e87545
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-11-22 11:04:11 -0300

    Se agrega token de login

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 3000
 const fs = require('fs')
 const path = require('path')
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 
 app.use(express.json())
+app.use(cors());
 
 
 app.get('/sell/:nombre', (req, res)=>{
@@ -112,6 +115,21 @@ app.get('/cart/:nombre', (req, res)=>{
         res.json(JSON.parse(data))
     })
 })
+
+app.post('/login', (req, res) => {
+
+    const SECRET_KEY = 'mi_acceso_JAP';
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Faltan credenciales' });
+    }
+    
+    const token = jwt.sign({ username }, SECRET_KEY);
+    res.json({ token });
+});
+
 
 app.listen(port, () =>{
     console.log(`Servidor corriendo en http://localhost:${port}`)
