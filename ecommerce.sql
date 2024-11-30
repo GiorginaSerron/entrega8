22c4eca9ef9c930f9a2f60e667b39f1c26a93f6b
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-11-30 20:58:01 -0300

    Update sql
    Se agregan los datos de producto a la correspondiente tabla

--- ecommerce.sql
+++ ecommerce.sql
@@ -26,7 +26,6 @@ CREATE TABLE IF NOT EXISTS `categorias` (
   UNIQUE KEY `CategoriaID` (`CategoriaID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Data exporting was unselected.
 
 -- Dumping structure for table ecommercedb.compras
 CREATE TABLE IF NOT EXISTS `compras` (
@@ -39,9 +38,9 @@ CREATE TABLE IF NOT EXISTS `compras` (
   KEY `ProductoID` (`ProductoID`),
   CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`NombreUsuario`) REFERENCES `usuarios` (`NombreUsuario`),
   CONSTRAINT `compras_ibfk_3` FOREIGN KEY (`ProductoID`) REFERENCES `productos` (`ProductoID`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Data exporting was unselected.
+
 
 -- Dumping structure for table ecommercedb.productos
 CREATE TABLE IF NOT EXISTS `productos` (
@@ -53,7 +52,22 @@ CREATE TABLE IF NOT EXISTS `productos` (
   UNIQUE KEY `ProductoID` (`ProductoID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table ecommercedb.productos: ~14 rows (approximately)
+INSERT INTO `productos` (`Nombre`, `unidadesVendidas`, `ProductoID`, `Descripcion`, `Precio`) VALUES
+	('Computadora de escritorio', 11, 40281, '-', 2599.00),
+	('Oso de peluche', 98, 50741, '-', 2400.00),
+	('Pelota de básquetbol', 11, 50742, '-', 2999.00),
+	('PlayStation 5', 16, 50743, '-', 59999.00),
+	('Bicicleta', 8, 50744, '-', 10999.00),
+	('Chevrolet Onix Joy', 35, 50921, '-', 13500.00),
+	('Fiat Way', 52, 50922, '-', 14500.00),
+	('Suzuki Celerio', 25, 50923, '-', 12500.00),
+	('Peugeot 208', 17, 50924, '-', 15200.00),
+	('Bugatti Chiron', 0, 50925, '-', 350000.00),
+	('Juego de comedor', 88, 60801, '-', 4000.00),
+	('Sofá', 13, 60802, '-', 24000.00),
+	('Armario', 24, 60803, '-', 8000.00),
+	('Mesa de centro', 37, 60804, '-', 10000.00);
 
 -- Dumping structure for table ecommercedb.producto_categoria
 CREATE TABLE IF NOT EXISTS `producto_categoria` (
@@ -65,7 +79,6 @@ CREATE TABLE IF NOT EXISTS `producto_categoria` (
   CONSTRAINT `ProductoID` FOREIGN KEY (`ProductoID`) REFERENCES `productos` (`ProductoID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Data exporting was unselected.
 
 -- Dumping structure for table ecommercedb.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -80,7 +93,6 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   UNIQUE KEY `NombreUsuario` (`NombreUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
